282b142776e56b46ef8dc8dd88f2275ae99c3bc9
aqib <aqibarshad>
2020-06-19 10:30:14 +0500

    datetime now saved in fire base

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,5 +1,7 @@
-import 'package:bongoo/ui/widgets/platform-widgets/platform_scaffold.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,10 +11,63 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return PlatformScaffold(
-        isAppBar: false,
-        child: Center(
-          child: Text('This is home page'),
-        ));
+       DateTime now = DateTime.now();
+      String formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+      createData(){
+    
+     DocumentReference documentReference=Firestore.instance.collection("DateTime").document(formattedDate);
+    //  print("******document id******* "+ds.documentID);
+      Map<String, dynamic> date={
+      "Date":formattedDate,
+};
+documentReference.setData(date).whenComplete((){
+  print("$formattedDate created");
+});
+}
+return MaterialApp(
+  home: DefaultTabController(
+    length: 3,
+    child: Scaffold(
+      appBar: AppBar(
+        bottom: TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.access_alarm),text: "Alarm",),
+            Tab(icon: Icon(Icons.access_time),text:"Clock" ,),
+            Tab(icon: Icon(Icons.timer),text:"Timer"),
+          ],
+        ),
+        title: Text('Tabs Demo'),backgroundColor: Colors.black,
+      ),
+      body: TabBarView(
+        children: [
+          Icon(Icons.access_alarm),
+          Center(child:
+          Column(children:<Widget>[
+          Text(formattedDate,textAlign: TextAlign.center,style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),),
+
+              SizedBox(
+              width: 100.0,
+                height: 50.0,
+               child:RaisedButton(
+                       
+                      textColor: Colors.white,
+              color: Colors.blue,   
+  //            //  padding: const EdgeInsets.all(8.0),
+              child: Text("Done"),
+              onPressed: (){
+                createData();
+    // Navigator.push(context, MaterialPageRoute(builder: (context)=>Player()));             
+              }
+            ),
+      ),
+      
+        ])),
+          Icon(Icons.timer),
+        ],
+        
+      ),
+    ),
+  ),
+);
   }
 }
